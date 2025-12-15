eac7016b27866d5e7e117c582c7a96a7974fa9e1
chetan singh rathore <chetan.singh2@arm.com>
2025-12-15 21:43:27 +0530

    Add null check before freeing g_smbios_info_table
    Add null check before freeing g_smbios_info_table

--- val/src/acs_pe_infra.c
+++ val/src/acs_pe_infra.c
@@ -540,7 +540,15 @@ val_get_num_smbios_slots()
 void
 val_smbios_free_info_table()
 {
-  pal_mem_free_aligned((void *)g_smbios_info_table);
+      if (g_smbios_info_table != NULL) {
+        pal_mem_free_aligned((void *)g_smbios_info_table);
+        g_smbios_info_table = NULL;
+    }
+    else {
+      val_print(ACS_PRINT_ERR,
+                  "\n WARNING: g_smbios_info_table pointer is already NULL",
+        0);
+    }
 }
 #endif
 
